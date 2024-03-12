175406b3a2b0864d452e0df8147196c9527679bd
Stefano Sanasi <68242764+TheStef56@users.noreply.github.com>
2024-03-12 14:13:41 +0100

    fixed audio buffering

--- screenrecorder.py
+++ screenrecorder.py
@@ -1,5 +1,5 @@
 import time, threading, pyautogui, cv2, numpy, os
-from moviepy.editor import AudioFileClip, VideoFileClip
+from moviepy.editor import AudioFileClip, VideoFileClip, vfx
 import soundcard
 import soundfile
 
@@ -19,6 +19,7 @@ class ScreenRecorder():
         self._fourcc = cv2.VideoWriter_fourcc(*"XVID")
         self._out = None
         self._num_frames = 0
+        self._audio_buffer = []
 
     def start_recording(self):
         if not os.path.exists("./recordings/"):
@@ -33,6 +34,7 @@ class ScreenRecorder():
         self._start_time = time.time()
 
     def stop_recording(self):
+        self._stop_time = time.time()
         self._recording = False
         self._out.release()
         self._save_video()
@@ -51,22 +53,37 @@ class ScreenRecorder():
             include_loopback=True).recorder(
                 samplerate=self._sample_rate,
                 channels=self._channels) as mic:
-            
             while self._recording:
+                self._audio_buffer.append(mic.record(numframes=self._sample_rate))
+               
+    def _save_video(self):
+        channelmap = list(range(self._channels))
+        required_frames = self._sample_rate*len(set(channelmap))
+        while len(self._audio_buffer) > 0: 
+            if len(self._audio_buffer[0])*len(self._audio_buffer) > required_frames:
+                buffer = self._audio_buffer[:required_frames]
+                self._audio_buffer = self._audio_buffer[required_frames:]
+                data = numpy.reshape(numpy.concatenate(buffer), [-1, len(set(channelmap))])
+                data = data[:, channelmap]
                 try:
                     buffer, samp = soundfile.read(f"./recordings/{self._prefix}.wav")
                 except:
                     buffer = []
-                data = mic.record(numframes=self._sample_rate)
                 buffer = list(buffer)
                 buffer += list(data)
                 soundfile.write(file=f"./recordings/{self._prefix}.wav", data=buffer, samplerate=self._sample_rate)
-    
-    def _save_video(self):
+            else:
+                break
+        actual_duration = (self._stop_time - self._start_time)
         video_clip = VideoFileClip(f"./recordings/{self._prefix}.avi")
-        audio_clip = AudioFileClip(f"./recordings/{self._prefix}.wav")
-        actual_frame_rate = self._num_frames / (time.time() - self._start_time)
-        final_clip = video_clip.set_audio(audio_clip)
-        final_clip = final_clip.set_duration(audio_clip.duration)  
+        actual_frame_rate = self._num_frames / actual_duration
+        video_clip = video_clip.fx(vfx.speedx, video_clip.duration / actual_duration)
+        if self._include_audio:
+            audio_clip = AudioFileClip(f"./recordings/{self._prefix}.wav")
+            final_clip = video_clip.set_audio(audio_clip)
+            final_clip = final_clip.set_duration(audio_clip.duration)  
+        else:
+            final_clip = video_clip
+            final_clip = final_clip.set_duration(video_clip.duration)  
         final_clip = final_clip.set_fps(actual_frame_rate)
         final_clip.write_videofile(f"./recordings/{self._prefix}.mp4", codec="libx264")
